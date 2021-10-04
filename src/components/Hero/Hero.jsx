ef34dc8a139a949d333b71e2afb5fbd0a123cfc9
EhWanu <cameronjshadgett@gmail.com>
2021-10-04 14:05:48 -0400

    hero layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,21 @@ import './Hero.scss'
 export default function Hero() {
     return (
         <div className='hero' id='hero'>
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Cameron</h1>
+                    <span></span>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
