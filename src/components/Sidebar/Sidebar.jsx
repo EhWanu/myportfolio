b8f930fad70f11e968cc3258ed1b4848ab4d0766
EhWanu <cameronjshadgett@gmail.com>
2021-10-03 23:29:13 -0400

    created sidebar componenet

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,23 @@ import './Sidebar.scss'
 export default function Sidebar() {
     return (
         <div className='sidebar' id='sidebar'>
-            
+            <ul>
+                <li>
+                    <a href="#hero">Home</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#servicex">Services</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testimonials</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>            
         </div>
     )
 }
