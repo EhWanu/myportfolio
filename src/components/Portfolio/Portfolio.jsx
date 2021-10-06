4ea27bcd6a5f233e3c2aa957a252efb49e0b8e03
EhWanu <cameronjshadgett@gmail.com>
2021-10-06 13:46:31 -0400

    skeleton layout

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,20 @@ import './Portfolio.scss'
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
-        
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
