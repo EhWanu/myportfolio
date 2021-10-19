7d040626ecaafd04148de28ca08245a5dc6522da
EhWanu <cameronjshadgett@gmail.com>
2021-10-19 11:38:13 -0400

    added image and background card

--- personalportfolio/src/components/about/About.jsx
+++ personalportfolio/src/components/about/About.jsx
@@ -1,7 +1,21 @@
 import "./about.css";
 
 const About = () => {
-	return <div className="about"></div>;
+	return (
+		<div className="a">
+			<div className="a-left">
+				<div className="a-card bg "></div>
+				<div className="a-card">
+					<img
+						src="https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+						alt=""
+						className="a-img"
+					/>
+				</div>
+			</div>
+			<div className="a-right"></div>
+		</div>
+	);
 };
 
 export default About;
