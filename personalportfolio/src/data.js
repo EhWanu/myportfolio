4d2f9c3059d58de8ba58d2244ccaab919fa2bedc
EhWanu <cameronjshadgett@gmail.com>
2021-10-19 14:23:39 -0400

    products /w placeholder imgs

---
+++ personalportfolio/src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+	{
+		id: 1,
+		img: "https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		link: "http://cameronshadgett.com",
+	},
+	{
+		id: 2,
+		img: "https://images.pexels.com/photos/3861959/pexels-photo-3861959.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		link: "http://cameronshadgett.com",
+	},
+	{
+		id: 3,
+		img: "https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		link: "http://cameronshadgett.com",
+	},
+	{
+		id: 4,
+		img: "https://images.pexels.com/photos/4709285/pexels-photo-4709285.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		link: "http://cameronshadgett.com",
+	},
+	{
+		id: 5,
+		img: "https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		link: "http://cameronshadgett.com",
+	},
+	{
+		id: 6,
+		img: "https://images.pexels.com/photos/360591/pexels-photo-360591.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		link: "http://cameronshadgett.com",
+	},
+];
