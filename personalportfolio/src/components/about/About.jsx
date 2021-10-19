f8ffc584fc8f3efc4b9ff423e4a76e2e9e447d5a
EhWanu <cameronjshadgett@gmail.com>
2021-10-19 11:59:39 -0400

    about section layout

--- personalportfolio/src/components/about/About.jsx
+++ personalportfolio/src/components/about/About.jsx
@@ -13,7 +13,22 @@ const About = () => {
 					/>
 				</div>
 			</div>
-			<div className="a-right"></div>
+			<div className="a-right">
+				<h1 className="a-title">About Me</h1>
+				<p className="a-sub">
+					It is a long established fact that a reader
+					will be distracted by the readable content.
+				</p>
+				<p className="a-desc">
+					Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua.
+					Ut enim ad minim veniam, quis nostrud
+					exercitation ullamco laboris nisi ut aliquip
+					ex ea commodo consequat duis aute irure dolor
+					in reprehende.
+				</p>
+			</div>
 		</div>
 	);
 };
